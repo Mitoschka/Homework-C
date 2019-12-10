03f2d4f4644a5b625f58ba1989d96b03effc23bf
Michael_200 <michael.eirich@mail.ru>
2019-12-10 17:16:49 +0300

    Добавил решение

--- Тест 2.1 v2.0/Тест 2.1 v2.0/Тест 2.1 v2.0.cpp
+++ Тест 2.1 v2.0/Тест 2.1 v2.0/Тест 2.1 v2.0.cpp
@@ -1,5 +1,8 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
+
+int const size = 1000;
 
 struct node 
 {
@@ -7,10 +10,11 @@ struct node
 	struct node* next;
 };
 
-node* list(int a)
+
+node* list(int element)
 {
 	node* head = new node;
-	head->data = a;
+	head->data = element;
 	head->next = NULL;
 	return (head);
 }
@@ -26,19 +30,43 @@ node* add(node* head, int val)
 	return (temp);
 }
 
-void printReversed(struct node* node)
+void openFile()
 {
-	if (node != nullptr) 
+	FILE* file = fopen("file.txt", "r");
+	if (file == nullptr)
 	{
-		printReversed(node->next);
-		printf("%d\n", node->data);
+		file = fopen("file.txt", "w");
+		fclose(file);
+		file = fopen("file.txt", "r");
 	}
+}
+
+void printFromFile(FILE* file)
+{
+	openFile();
+	char string[size];
+	int numOfLines = 0;
+	if ((file = fopen("file.txt", "r")) == nullptr)
+	{
+		printf("error\n");
+	}
+	else
+	{
+		while (fgets(string, size, file))
+		{
+			printf("%s", string);
+			numOfLines++;
+		}
+	}
+	printf("Количество строк: %d\n", numOfLines);
+	fclose(file);
+	printf("Все данные были распечатаны\n");
 }
 
 bool search(node* list, int data)
 {
 	node* ptr = list;
-	while (ptr != NULL)
+	while (ptr != nullptr)
 	{
 		if (ptr->data == data)
 		{
@@ -49,12 +77,31 @@ bool search(node* list, int data)
 	return false;
 }
 
-int main(struct node* node)
+void printReversed(struct node* node)
 {
 	if (node != nullptr)
 	{
 		printReversed(node->next);
 		printf("%d\n", node->data);
 	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	FILE* file = fopen("file.txt", "r+t");
+	printFromFile(file);
+	int N = 0;
+	int data = 0;
+	for (int i = 0; i < N; i++)
+	{
+		printf("Input %i element", i + 1);
+		node* data = (node*)malloc(sizeof(node));
+		scanf("%d", data);
+		add(data, i);
+	}
+	int result = 0;
+	result = printReversed(node);
+	printf("\n\nResult: %d\n", result);
 	return 0;
 }
