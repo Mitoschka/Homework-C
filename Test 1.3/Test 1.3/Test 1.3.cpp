0e607da997e24b67cf3424c813ca655fa229e4f8
Michael_200 <michael.eirich@mail.ru>
2019-12-24 16:47:27 +0300

    Добавил решение задачи

---
+++ Test 1.3/Test 1.3/Test 1.3.cpp
@@ -0,0 +1,44 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+void record()
+{
+	FILE* firstFile = nullptr;
+	if ((firstFile = fopen("text.txt", "r")) == nullptr)
+	{
+		printf("Could not open file...");
+		exit(0);
+	}
+	int readSymbol = 0;
+	int i = 0;
+	FILE* secondFile = nullptr;
+	if ((secondFile = fopen("quotes.txt", "r")) == nullptr)
+	{
+		printf("Could not open file...");
+		exit(0);
+	}
+	secondFile = fopen("quotes.txt", "w");
+	while ((readSymbol = fgetc(firstFile)) != EOF)
+	{
+		if (readSymbol == '\"')
+		{
+			if (++i % 2)
+			{
+				fprintf(secondFile, "\n");
+			}
+			fprintf(secondFile, "\"");
+		}
+		else if (i % 2)
+		{
+			fprintf(secondFile, "%c", readSymbol);
+		}
+	}
+	fclose(firstFile);
+	fclose(secondFile);
+}
+
+int main()
+{
+	record();
+	return 0;
+}
