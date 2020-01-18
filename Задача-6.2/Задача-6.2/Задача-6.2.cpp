a1549b2ea686eec0eca7c03f56d256101f043513
Michael_200 <michael.eirich@mail.ru>
2020-01-19 00:09:00 +0300

    добавил 6.2

---
+++ Задача-6.2/Задача-6.2/Задача-6.2.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int const N = 1000;
+
+void stringBalance()
+{
+	printf("Enter the string: ");
+	char str[N];
+	fgets(str, N, stdin);
+	printf("\nThe string: ");
+	puts(str);
+}
+
+int main()
+{
+	stringBalance();
+	return 0;
+}
